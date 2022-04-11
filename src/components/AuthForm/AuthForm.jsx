ae751b871211fd35264902949ad01b9fff2cc9ab
Nickolas Barbosa <81327961+nicko-dev@users.noreply.github.com>
2022-04-11 00:34:28 -0300

    routes, modals, layout

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -3,23 +3,27 @@ import { Avatar, Button, Paper, Grid, Typography, Container } from '@mui/materia
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import { useDispatch, useSelector } from 'react-redux';
 import { useForm } from 'react-hook-form';
-import { login, singUp } from '../../storeConfig/slices/authSlice';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { clearAuthMsg, login, singUp } from '../../storeConfig/slices/authSlice';
 
 import Input from './Input';
+import userSchema from './userSchema';
+import loginSchema from './loginSchema';
+import { useNavigate } from 'react-router-dom';
 
 const AuthForm = () => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const message = useSelector(state => state.auth.message);
+    const user = useSelector(state => state.auth?.user)
 
-    const {
-        register,
-        handleSubmit,
-        reset,
-        formState: { errors },
-    } = useForm();
     const [showPassword, setShowPassword] = useState(false);
     const [showConfirmPassword, setShowConfirmPassword] = useState(false);
     const [isSignUp, setIsSignup] = useState(true);
+    
+    const { register, handleSubmit, reset, formState: { errors } } = useForm({
+        resolver: !isSignUp ? yupResolver(userSchema) : yupResolver(loginSchema),
+    });
 
     const onSubmit = data => {
         isSignUp ? dispatch(login(data)) : dispatch(singUp(data));
@@ -28,6 +32,7 @@ const AuthForm = () => {
     const switchMode = () => {
         setIsSignup(!isSignUp);
         setShowPassword(false);
+        dispatch(clearAuthMsg())
         reset();
     };
 
@@ -45,18 +50,18 @@ const AuthForm = () => {
                     <Grid container spacing={2} justifyContent='center' sx={{ paddingY: 3 }}>
                         {!isSignUp && (
                             <>
-                                <Input name='firstName' label='Nome  *' type='text' autoFocus={isSignUp ? false : true} half register={register} />
-                                <Input name='lastName' label='Sobrenome *' type='text' half register={register} />
+                                <Input name='firstName' label='Nome  *' type='text' autoFocus={isSignUp ? false : true} half register={register} errors={errors} />
+                                <Input name='lastName' label='Sobrenome *' type='text' half register={register} errors={errors} />
                             </>
                         )}
-                        <Input name='email' label='Email *' type='text' autoFocus={!isSignUp ? false : true} register={register} />
-                        <Input name='password' label='Password *' register={register} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
+                        <Input name='email' label='Email *' type='text' autoFocus={!isSignUp ? false : true} register={register} errors={errors} />
+                        <Input name='password' label='Password *' register={register} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} errors={errors} />
                         {!isSignUp && (
                             <>
-                                <Input name='confirmPassword' label='Corfirmação de Password *' register={register} type={showConfirmPassword ? 'text' : 'password'} handleShowPassword={handleShowConfirmPassword} />
+                                <Input name='confirmPassword' label='Corfirmação de Password *' register={register} type={showConfirmPassword ? 'text' : 'password'} handleShowPassword={handleShowConfirmPassword} errors={errors} />
                             </>
                         )}
-                        <Grid item >
+                        <Grid item>
                             <Typography variant={'body1'} sx={{ color: message?.status === 'success' ? 'blue' : 'red', marginBottom: 2 }}>
                                 {message?.msg}
                             </Typography>
